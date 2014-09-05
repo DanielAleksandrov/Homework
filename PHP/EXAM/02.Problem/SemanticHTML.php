3bd4763a465bf94c3d2879f6de21a68f603b1556
iuli1478 <iuli.iuli17@gmail.com>
2014-09-06 00:52:48 +0300

    Deleted old files

--- PHP/EXAM/02.Problem/SemanticHTML.php
+++
@@ -1,6 +0,0 @@
-<?php
-$textInput = htmlspecialchars($_GET['html']);
-$arr = preg_split('/<div/g' ,$textInput);
-var_dump($arr);
-
-?>
--- PHP/UP/uf_cfg.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$max_file_size = 10; // Размера в KB !
-	$upload_path = "\\wamp\\www\\uploads";
-	$upload_slash = "\\"; 
-	
-?>
